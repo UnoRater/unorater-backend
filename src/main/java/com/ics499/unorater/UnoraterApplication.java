088acc640100f3c96ba2d360d82b0835baa6e23a
Bossmanswift <olaoluwaseyi81@gmail.com>
2020-05-17 02:09:28 -0500

    Initial commit

---
+++ src/main/java/com/ics499/unorater/UnoraterApplication.java
@@ -0,0 +1,13 @@
+package com.ics499.unorater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoraterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoraterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ics499/unorater/UnoraterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ics499.unorater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoraterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
