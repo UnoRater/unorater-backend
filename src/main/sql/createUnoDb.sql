3ceaa3f6cad218199cfd6a8d9e8e2c3ccc162d1f
Bossmanswift <olaoluwaseyi81@gmail.com>
2020-05-24 16:38:36 -0500

    Update db script

--- src/main/sql/createUnoDb.sql
+++ src/main/sql/createUnoDb.sql
@@ -1,17 +1,21 @@
+/** Create database is one does not exist */
 IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'unodatabase')
     BEGIN
         CREATE DATABASE [unodatabase]
     END;
 GO
 
+/** Specify database to use */
 USE [unodatabase]
 GO
 
+/** Drop tables if they exist in the database */
 DROP TABLE IF EXISTS dbo.[Reviews]
 DROP TABLE IF EXISTS dbo.[Services]
 DROP TABLE IF EXISTS dbo.[Departments]
 DROP TABLE IF EXISTS dbo.[Users]
 
+/** Create tables */
 CREATE TABLE
     dbo.Departments
 (
@@ -24,11 +28,12 @@ CREATE TABLE
     dbo.Users
 (
     userId BIGINT PRIMARY KEY NOT NULL IDENTITY(1,1),
-	userName VARCHAR(20) NOT NULL,
-	passwordHash BINARY(64) NOT NULL,
-	salt UNIQUEIDENTIFIER NOT NULL,
-	email VARCHAR (20) NOT NULL,
-	dateCreated DATETIME NOT NULL
+    userName VARCHAR(20) NOT NULL,
+    passwordHash BINARY(64) NOT NULL,
+    salt UNIQUEIDENTIFIER NOT NULL,
+    email VARCHAR (20) NOT NULL,
+    isAdmin BIT NOT NULL,
+    dateCreated DATETIME NOT NULL
 )
 GO
 
@@ -38,10 +43,10 @@ CREATE TABLE
     serviceId BIGINT PRIMARY KEY NOT NULL IDENTITY(1,1),
     userId BIGINT NOT NULL,
     departmentId BIGINT NOT NULL,
-	serviceName VARCHAR(50) NOT NULL,
-	dateCreated DATETIME NOT NULL,
-	FOREIGN KEY (userId) REFERENCES Users(userId),
-	FOREIGN KEY (departmentId) REFERENCES Departments(departmentId)
+    serviceName VARCHAR(50) NOT NULL,
+    dateCreated DATETIME NOT NULL,
+    FOREIGN KEY (userId) REFERENCES Users(userId),
+    FOREIGN KEY (departmentId) REFERENCES Departments(departmentId)
 )
 GO
 
@@ -51,34 +56,36 @@ CREATE TABLE
     reviewId BIGINT PRIMARY KEY NOT NULL IDENTITY(1,1),
     userId BIGINT NOT NULL,
     serviceId BIGINT NOT NULL,
-	reviewText VARCHAR(500) NOT NULL,
-	dateCreated DATETIME NOT NULL,
-	dateModified DATETIME NULL,
-	FOREIGN KEY (userId) REFERENCES Users(userId),
-	FOREIGN KEY (serviceId) REFERENCES [Services](serviceId)
+    reviewText VARCHAR(500) NOT NULL,
+    dateCreated DATETIME NOT NULL,
+    dateModified DATETIME NULL,
+    FOREIGN KEY (userId) REFERENCES Users(userId),
+    FOREIGN KEY (serviceId) REFERENCES [Services](serviceId)
 )
 GO
 
+/** Stored procedures for user authentication */
 DROP PROCEDURE IF EXISTS dbo.spAddUser
 GO
 
 CREATE PROCEDURE dbo.spAddUser
-    @puserName VARCHAR(20), 
-    @ppassword VARCHAR(50), 
-	@pemail VARCHAR(50),
-	@pdateCreated DATETIME,
+    @puserName VARCHAR(20),
+    @ppassword VARCHAR(50),
+    @pemail VARCHAR(50),
+    @pdateCreated DATETIME,
+    @pisAdmin BIT,
     @responseMessage VARCHAR(250) OUTPUT
 AS
 BEGIN
     SET NOCOUNT ON
-	DECLARE @salt UNIQUEIDENTIFIER = NEWID()
+    DECLARE @salt UNIQUEIDENTIFIER = NEWID()
     BEGIN TRY
-        INSERT INTO dbo.[Users] (userName, passwordHash, salt, email, dateCreated)
-        VALUES(@puserName, HASHBYTES('SHA2_512', @ppassword+CAST(@salt AS VARCHAR(36))), @salt, @pemail, @pdateCreated)
+        INSERT INTO dbo.[Users] (userName, passwordHash, salt, email, isAdmin, dateCreated)
+        VALUES(@puserName, HASHBYTES('SHA2_512', @ppassword+CAST(@salt AS VARCHAR(36))), @salt, @pemail, @pisAdmin, @pdateCreated)
         SET @responseMessage='Success'
     END TRY
     BEGIN CATCH
-        SET @responseMessage=ERROR_MESSAGE() 
+        SET @responseMessage=ERROR_MESSAGE()
     END CATCH
 END
 GO
@@ -88,22 +95,46 @@ GO
 CREATE PROCEDURE dbo.spLogin
     @puserName VARCHAR(20),
     @ppassword VARCHAR(50),
-    @responseMessage NVARCHAR(250)='' OUTPUT
+    @responseMessage VARCHAR(250)='' OUTPUT
 AS
 BEGIN
     SET NOCOUNT ON
     DECLARE @userId INT
     IF EXISTS (SELECT TOP 1 userId FROM [dbo].[Users] WHERE user=@puserName)
-    BEGIN
-        SET @userID=(SELECT userId FROM [dbo].[Users] WHERE userName=@puserName AND passwordHash=HASHBYTES('SHA2_512', @ppassword+CAST(Salt AS NVARCHAR(36))))
+        BEGIN
+            SET @userID=(SELECT userId FROM [dbo].[Users] WHERE userName=@puserName AND passwordHash=HASHBYTES('SHA2_512', @ppassword+CAST(Salt AS VARCHAR(36))))
 
-       IF(@userID IS NULL)
-           SET @responseMessage='Incorrect password'
-       ELSE 
-           SET @responseMessage='User successfully logged in'
-    END
+            IF(@userID IS NULL)
+                SET @responseMessage='Incorrect password'
+            ELSE
+                SET @responseMessage='User successfully logged in'
+        END
     ELSE
-       SET @responseMessage='Invalid login'
+        SET @responseMessage='Invalid login'
+END
+GO
+
+DROP PROCEDURE IF EXISTS dbo.spAdminLogin
+GO
+CREATE PROCEDURE dbo.spAdminLogin
+    @puserName VARCHAR(20),
+    @ppassword VARCHAR(50),
+    @responseMessage VARCHAR(250)='' OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON
+    DECLARE @userId INT
+    IF EXISTS (SELECT TOP 1 userId FROM [dbo].[Users] WHERE user=@puserName)
+        BEGIN
+            SET @userID=(SELECT userId FROM [dbo].[Users] WHERE userName=@puserName AND passwordHash=HASHBYTES('SHA2_512', @ppassword+CAST(Salt AS VARCHAR(36))) AND isAdmin = 1)
+
+            IF(@userID IS NULL)
+                SET @responseMessage='Incorrect password'
+            ELSE
+                SET @responseMessage='User successfully logged in'
+        END
+    ELSE
+        SET @responseMessage='Invalid login'
 END
 
 
