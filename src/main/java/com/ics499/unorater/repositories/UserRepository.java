bec17a6929513f7e1e446950d42094898886965e
Bossmanswift <olaoluwaseyi81@gmail.com>
2020-07-19 18:59:25 -0500

    Implement custom search query

--- src/main/java/com/ics499/unorater/repositories/UserRepository.java
+++ src/main/java/com/ics499/unorater/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.ics499.unorater.repositories;
 
 import com.ics499.unorater.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -25,4 +27,7 @@ public interface UserRepository extends JpaRepository <User, Integer> {
     Boolean existsByuserName(String userName);
 
     Boolean existsByEmail(String email);
+
+    @Query("select u from User u where u.userName like %:userName")
+    List<User> searchUsers (@Param("userName") String userName);
 }
