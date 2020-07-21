9dc52f6541c47b7038015192b6d76f9f87d1452e
Bossmanswift <olaoluwaseyi81@gmail.com>
2020-07-20 23:29:06 -0500

    Require no auth for hello endpoint

--- src/main/java/com/ics499/unorater/controllers/HelloController.java
+++ src/main/java/com/ics499/unorater/controllers/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/hello")
 @RestController
 public class HelloController {
 
